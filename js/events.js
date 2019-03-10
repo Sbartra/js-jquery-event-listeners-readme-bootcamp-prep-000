e3925453bfc748508e8fa4cc9d7db8835944d482
Segundo Bartra <segundobartra@gmail.com>
2019-03-10 23:46:40 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -19,3 +19,11 @@ function pressIt(){
     }
   });
 }
+
+function submitIt(){
+  $("form").on("submit", function() {
+  if ($( "input:first" ).val() === "correct") {
+    alert('your form is going to be submitted now');
+  }
+});
+}
